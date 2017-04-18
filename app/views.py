b64f0e8e42c0005bf888513967ed32b361657f2f
Nick Westin <nicolaswestin@gmail.com>
2017-04-17 21:17:38 -0400

    Issue data is displayed properly. Bug with rounding of issue number only affects placement and routing of individual issue once clicked.

--- app/views.py
+++ app/views.py
@@ -16,22 +16,39 @@ def sql_get(sql):
         return result
 
 
+@app.context_processor
+def utility_processor():
+    def sql_query_grab(sql_query):
+        return sql_get(sql_query)
+
+    return dict(sql_get=sql_query_grab)
+
+
 @app.route('/')
 def index():
-    series = sql_get('SELECT * from series')
-    series_id_list = sql_get('SELECT seriesID from series')
-    series_to_id = zip(series_id_list, series)
+    series = sql_get(
+        'SELECT series.seriesID, series.seriesName, series.releaseDate, '
+        'series.endDate, series.numOfIssues, issue.imageURL FROM series '
+        'INNER JOIN issue ON issue.seriesName = series.seriesName '
+        'GROUP BY series.seriesName ORDER BY seriesID'
+    )
     year = series[0]['releaseDate'].year
     return render_template('index.html',
                            page_title='Home - All Series',
-                           series_list=series_to_id,
+                           series_list=series,
                            year=year)
 
 
 @app.route('/series/<series_id>/')
 def issues(series_id):
+    issue_list = sql_get(
+        'SELECT * FROM issue INNER JOIN series '
+        'ON issue.seriesName = series.seriesName '
+        'WHERE series.seriesID = {} ORDER BY issue.issueNum'.format(series_id)
+    )
     return render_template('series.html',
-                           page_title=series_name)
+                           issue_data=issue_list,
+                           page_title='{} Issues'.format(issue_list[0]['seriesName']))
 
 
 @app.route('/issue/<issue_id>')
